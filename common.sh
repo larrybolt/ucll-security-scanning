23c2cfe0abe7e0d6b60ebde944cad7e856c6fe49
Larry Boltovskoi <larry.bolt@gmail.com>
2018-01-07 21:36:05 +0100

    Get base ip address

--- common.sh
+++ common.sh
@@ -26,3 +26,30 @@ function ::netmask2cidr()
 
   echo "${len}"
 }
+
+# source: https://forum.openwrt.org/viewtopic.php?pid=220781#p220781
+mask2cdr ()
+{
+   # Assumes there's no "255." after a non-255 byte in the mask
+   local x=${1##*255.}
+   set -- 0^^^128^192^224^240^248^252^254^ $(( (${#1} - ${#x})*2 )) ${x%%.*}
+   x=${1%%$3*}
+   echo $(( $2 + (${#x}/4) ))
+}
+
+
+cdr2mask ()
+{
+   # Number of args to shift, 255..255, first non-255 byte, zeroes
+   set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+   [ $1 -gt 1 ] && shift $1 || shift
+   echo "${1-0}.${2-0}.${3-0}.${4-0}"
+}
+
+# source: https://stackoverflow.com/a/15429733
+networkip ()
+{
+  IFS=. read -r i1 i2 i3 i4 <<< "$1"
+  IFS=. read -r m1 m2 m3 m4 <<< "$2"
+  printf "%d.%d.%d.%d\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))"
+}
--- scan.sh
+++ scan.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 source ./common.sh
 #::netmask2cidr
+#mask2cdr
 
 # Get the public ip address for this network
 PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
@@ -9,15 +10,23 @@ echo "PUBLIC IP: $PUBLIC_IP"
 # Get local ip addresses
 # Probably the first is the one we want, but scan all for good measure
 LOCAL_IPS=$(ifconfig \
-	| grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]{1,3} netmask (0x[0-f]{8})' \
-	| grep -Eo '([0-9]*\.){3}[0-9].*(0x[0-f]{8})' \
+	| grep -Eo '( *)?inet (addr:)?([0-9]*\.){3}[0-9]{1,3}\s+netmask ((0x[0-f]{8})(.*)|([0-9]*\.){3}[0-9]{1,3})' \
+	| grep -Eo '([0-9]*\.){3}[0-9].*((0x[0-f]{8})|([0-9]*\.){3}[0-9]{1,3})' \
 	| grep -v '127.0.0.1')
 
 IFS=$'\n'
 for _IP in $LOCAL_IPS; do
 	IP="$(echo $_IP | cut -d' ' -f1)"
 	NETMASK="$(echo $_IP | cut -d' ' -f3)"
-	CIDR="$(::netmask2cidr $NETMASK)"
+	if [ $NETMASK == 'netmask' ]; then
+		# probably running on kali
+		NETMASK="$(echo $_IP | cut -d' ' -f4)"
+		CIDR="$(mask2cdr $NETMASK)"
+	else
+		CIDR="$(::netmask2cidr $NETMASK)"
+		NETMASK="$(cdr2mask $CIDR)"
+	fi
+	BASEIP="$(networkip $IP $NETMASK)"
 
-	echo "Scanning IP: $IP/$CIDR"
+	echo "Scanning IP: $BASEIP/$CIDR (own ip: $IP)"
 done
